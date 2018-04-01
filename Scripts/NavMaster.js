7676fd40623c003b41cb4676c39b9585844a465b
kubotron17 <kubotron17@gmail.com>
2018-04-01 18:42:37 +0100

    Migration

--- Scripts/NavMaster.js
+++ Scripts/NavMaster.js
@@ -1,6 +1,6 @@
 function Nav1Click(){
-  document.location.href = 'http://localhost/index.html';
+  document.location.href = "http://ec2-35-178-61-185.eu-west-2.compute.amazonaws.com/index.html";
 }
 function Nav2Click(){
-  document.location.href = 'http://localhost/about.html';
+  document.location.href = "http://ec2-35-178-61-185.eu-west-2.compute.amazonaws.com/about.html";
 }
